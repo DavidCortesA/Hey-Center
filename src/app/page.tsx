33fa4c524648ce1f5b3dd3197573427a847b75f3
David Cortez <david.cortes.ayala@hotmail.com>
2024-11-04 07:05:14 -0600

    add:async data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,32 @@
+'use client'
+import React,{ useEffect, useState } from "react";
 import BodyContent from "@/components/common/BodyContent";
 import SideChat from "@/components/SideChat";
 import SideChatCard from "@/components/SideChatCard";
 
 export default function Home() {
+  const [data, setData] = useState<any>();
+
+  useEffect(()=>{
+    const fetchData = async () => {
+      try{
+        const getData = await fetch('https://sellia-files.s3.us-east-2.amazonaws.com/challenge/629e39e8b2d31319081e0650.json')
+        const dataJson = await getData.json();
+        setData(dataJson);
+      } catch(error){
+        console.log(error);
+      }
+    }
+    
+    fetchData();
+  },[])
+
+  console.log(data)
+
   return (
     <main className="w-full h-full flex flex-row flex-nowrap p-5">
       <BodyContent>
-        <SideChatCard/>
+        <SideChatCard data={data}/>
         <SideChat/>
       </BodyContent>
     </main>
--- src/components/SideChatCard.tsx
+++ src/components/SideChatCard.tsx
@@ -1,12 +1,49 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-const SideChatCard = () => {
+const SideChatCard = ({data}:{data: any}) => {
+  const [client, setClient] = useState<any>();
+  const [clientInfo, setClientInfo] = useState<any>(null);
+
+  useEffect(()=>{
+    const fetchData = async () => {
+      try{
+        const getData = await fetch('https://sellia-files.s3.us-east-2.amazonaws.com/challenge/clients.json');
+        const dataJson = await getData.json();
+        setClient(dataJson);
+      } catch(error){
+        console.log(error);
+      }
+    }
+
+    fetchData();
+  },[]);
+
+  useEffect(() => {
+    if (data && client.length > 0) {
+      // Encontrar el cliente correspondiente al client ID en `data`
+      const foundClient = client.find((item: any) => item.id === data.client);
+      setClientInfo(foundClient);
+    }
+  }, [data, client]);
+
   return (
-    <div className="w-1/5 p-4">
-      <button type='button'>Chats</button>
-      <button type='button'>Historial</button>
-    </div>
-  )
+    <div className="w-full">
+      {clientInfo && data.messages && data.messages.length > 0 ? (
+        <div>
+          <h2>Cliente: {clientInfo.name}</h2>
+          <p>Email: {clientInfo.email}</p>
+          <h3>Mensajes:</h3>
+          <ul>
+            {data.messages.map((message: any, index: number) => (
+              <li key={index}>{message.content}</li>
+            ))}
+          </ul>
+        </div>
+      ) : (
+        <p>No hay mensajes del cliente.</p>
+      )}
+    </div> 
+  );
 }
 
 export default SideChatCard
